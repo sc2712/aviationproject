391d2f3f29210c3dd03865ba888f297ee9931b7c
sc2712 <scclarktlll@gmail.com>
2025-04-28 13:14:02 +0100

    Your commit message

--- model_logic.py
+++ model_logic.py
@@ -1,6 +1,8 @@
 import numpy as np
 from tensorflow.keras.models import load_model as keras_load_model
 from tensorflow.keras.preprocessing import image
+import streamlit as st
+import os
 
 class_labels = [
     'fake commercial aircraft (AI)',
@@ -11,8 +13,22 @@ class_labels = [
     'real private aircraft'
 ]
 
-def load_model(model_path):
-    return keras_load_model(model_path)
+def load_model_from_path(model_path):
+    #Loads the Keras model from the specified path.
+    try:
+        # Ensure the file exists
+        if not os.path.exists(model_path):
+            raise FileNotFoundError(f"Model file not found at {model_path}")
+        
+        model = keras_load_model(model_path)
+        st.success("Model loaded successfully.")
+        return model
+    except FileNotFoundError as e:
+        st.error(f"Error: {e}")
+        return None
+    except Exception as e:
+        st.error(f"Error loading model: {e}")
+        return None
 
 def preprocess_image(image_file, img_size=(224, 224)):
     img = image.load_img(image_file, target_size=img_size)
